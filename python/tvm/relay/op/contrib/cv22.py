dbdc0668398f3e1c90ed187efc2e034e7a469b47
alexwong <11878166+alexwong@users.noreply.github.com>
2021-01-19 12:08:11 -0800

    Fix some merge/rebase issues

--- python/tvm/relay/op/contrib/cv22.py
+++ python/tvm/relay/op/contrib/cv22.py
@@ -34,7 +34,7 @@ def _register_external_op_helper(op_name, supported=True):
         A function that returns if the operator is supported by CV22.
     """
     @tvm.ir.register_op_attr(op_name, "target.cv22")
-    def _func_wrapper(attrs, args):
+    def _func_wrapper(attrs):
         return supported
 
     return _func_wrapper
